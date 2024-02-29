da2ed1fffc08c4c5e91e154328bed303a236bc19
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-01 01:23:11 +0700

    Optimize a little bit

--- leetcode/go/TwoSum/main.go
+++ leetcode/go/TwoSum/main.go
@@ -3,16 +3,17 @@ package main
 func main() {
 }
 func twoSum(nums []int, target int) []int {
-	var newArr []int
+	// var newArr []int
 	for i := 0; i < len(nums); i++ {
 		for j := len(nums) - 1; j > i; j-- {
 			// if nums[i] <= target && nums[j] <= target {
 			if nums[i]+nums[j] == target {
-				newArr = []int{i, j}
+				return []int{i, j}
 				break
 			}
 			// }
 		}
 	}
-	return newArr
+	// return newArr
+	return []int{}
 }
